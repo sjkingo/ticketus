14dd3c497bb4bdc0c3736fdd3e3168a7241b1138
Sam Kingston <sam@sjkwi.com.au>
2014-12-29 06:57:49 +1000

    Tidy wsgi.py file

--- ticketus/wsgi.py
+++ ticketus/wsgi.py
@@ -5,11 +5,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ticketus.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ticketus.settings')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-# Apply any WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
